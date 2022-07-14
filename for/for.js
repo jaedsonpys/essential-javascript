04aabd1c5442a812beda9bd34ce0bee88c37618f
Jaedson Silva <contacanal798@gmail.com>
2022-07-14 15:50:07 -0300

    adding for/

---
+++ for/for.js
@@ -0,0 +1,11 @@
+const persons = ["uva", "pera", "maçã"];
+
+// retorna apenas o index
+for(i in persons) {
+    console.log(i);
+}
+
+// retorna apenas o valor
+for(v of persons) {
+    console.log(v);
+}
